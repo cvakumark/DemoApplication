268d4553a54a33e1b48eeab4467d2e5ed5eb9aa6
kalva.s <kalva.s@hcl.com>
2018-07-06 15:11:44 +0530

    committed

--- UploadArtifact.ps1
+++ UploadArtifact.ps1
@@ -6,7 +6,7 @@ param(
 function UploadArtifacts($version) {
 #cd $env:workspace
 $URI = New-Object System.Uri("http://localhost:8081/artifactory/DemoApplication/DemoApplication_$version.zip")  
-#$SOURCE = "./DemoApplication_$version.zip"
+$SOURCE = "./DemoApplication_$version.zip"
 $AF_USER = "admin"  
 $AF_PWD = ConvertTo-SecureString "admin123" -AsPlainText -Force  
 $CREDS = New-Object System.Management.Automation.PSCredential ($AF_USER, $AF_PWD)  
